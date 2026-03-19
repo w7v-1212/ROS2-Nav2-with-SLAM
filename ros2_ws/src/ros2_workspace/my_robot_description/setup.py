2bcfd588fdeda0f75aa8fcbc379a050bbd9a88b5
g-goh <w0c7p125@gmail.com>
2026-03-20 01:16:49 +0800

    Add files via upload

---
+++ ros2_ws/src/ros2_workspace/my_robot_description/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'my_robot_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/urdf', glob('urdf/*')),
+        ('share/' + package_name + '/launch', glob('launch/*')),
+        ('share/' + package_name + '/config', glob('config/*')),
+        ('share/' + package_name + '/worlds', glob('worlds/*')),
+        ('share/' + package_name + '/maps', glob('maps/*')),
+        ('share/' + package_name + '/rviz', glob('rviz/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='gohwei',
+    maintainer_email='gohwei@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
