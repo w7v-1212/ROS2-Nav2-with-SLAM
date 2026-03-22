40ff0a42eca95218e3c872e894d4f465d9f96649
g-goh <w0c7p125@gmail.com>
2026-03-23 00:13:19 +0800

    Add files via upload

---
+++ ros2_ws/src/ros2_workspace/my_robot_bringup/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'my_robot_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*')),
+        ('share/' + package_name + '/config', glob('config/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='gohwei',
+    maintainer_email='gohwei@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
